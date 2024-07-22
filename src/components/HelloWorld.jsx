a4b7588cf6e4e678cf1467707b4cd58fb0a824ec
Rafael <rafaelandersonpg@gmail.com>
2024-07-21 22:41:57 -0500

    feat: dependency PropTypes

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,13 +1,23 @@
 import { Fragment } from "react";
+import PropTypes from "prop-types";
 
-export const HelloWorld = ({ user, id }) => {
+export const HelloWorld = ({ user, id, title }) => {
+  console.log(title);
   //const name = "Rafael";
+
   return (
     <Fragment>
-      <h1>Hola Mundo</h1>
+      <h1>{title}</h1>
+      {JSON.stringify(user)}
       <div>
-        que tal! {user} con el id {id}
+        que tal! {user.name} {user.lastName} con el id {id}
       </div>
     </Fragment>
   );
+};
+
+HelloWorld.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  user: PropTypes.object.isRequired,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { HelloWorld } from "./components/HelloWorld";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorld user={"Rafael"} id="1" />
+    <HelloWorld
+      user={{ name: "Rafael", lastName: "Ponte" }}
+      id={1}
+      title="Hola Mundo"
+    />
   </React.StrictMode>
 );
